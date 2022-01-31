d1061f9279f5c03a3dc68164305c870e0203d959
loafz <mathewbuck38@gmail.com>
2022-01-30 20:28:58 -0800

    First Flutter App

---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+          home: Scaffold(
+            backgroundColor: Colors.blueGrey,
+            appBar: AppBar(
+              title: const Text('I Am Rich'),
+              centerTitle: true,
+              backgroundColor: Colors.blueGrey[900],
+            ),
+            body: const Center(
+              child: Image(
+                image:
+                  AssetImage('images/diamond.png')
+              ),
+            ),
+          ),
+      ),
+  );
+}
+
